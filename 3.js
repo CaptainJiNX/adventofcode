c521824c895a0e2522b6903e095ad8b56f04da52
Roger Wilson <roger.wilson@tretton37.com>
2016-06-01 01:18:57 +0200

    Day 3 complete

---
+++ 3.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const H = require('highland');
+const getChars = require('./getChars');
+
+const pos = (x, y) => ({ x, y });
+const house = (pos) => `${pos.x}:${pos.y}`;
+
+const movement = {
+	'v': pos(0, 1),
+	'>': pos(1, 0),
+	'<': pos(-1, 0),
+	'^': pos(0, -1)
+};
+
+const toMovement = (x) => movement[x];
+const getMovements = () => getChars('input-3.txt').map(toMovement);
+
+const initHouse = (who, acc) => {
+	acc[who] = pos(0, 0);
+	return house(acc[who]);
+};
+
+const visitHouse = (who) => (acc, move) => {
+	const h = (acc[who] && house(acc[who])) || initHouse(who, acc);
+	acc[who].x += move.x;
+	acc[who].y += move.y;
+	acc.houses[h] = (acc.houses[h] || 1) + 1;
+	return acc;
+};
+
+const countHouses = (s) => s.pluck('houses')
+	.flatMap(H.keys)
+	.collect()
+	.pluck('length');
+
+// first half
+getMovements()
+	.reduce({ houses: {} }, visitHouse('santa'))
+	.through(countHouses)
+	.each(H.log);
+
+// second half
+
+const santa = visitHouse('santa');
+const robo = visitHouse('robo');
+const visitHouses = (acc, moves) => santa(robo(acc, moves[1]), moves[0]);
+
+getMovements()
+	.batch(2)
+	.reduce({ houses: {} }, visitHouses)
+	.through(countHouses)
+	.each(H.log);
